009f3633bac4d829dbc7f6c43ddadc994a65970e
marcofenoglio <marco.fenoglio@gmail.com>
2020-02-20 13:48:25 +0100

    added Function MyEnableRemoteDesktop

--- MyTweak.psm1
+++ MyTweak.psm1
@@ -104,6 +104,54 @@ Function MyEnableShowRibbon {
 	Set-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Ribbon" MinimizedStateTabletModeOff 0
 }
 
+# Allows Remote Desktop access to machine and enables Remote Desktop firewall rule
+# From Boxstarter.WinConfig/Enable-RemoteDesktop.ps1
+Function MyEnableRemoteDesktop {
+<#
+.SYNOPSIS
+Allows Remote Desktop access to machine and enables Remote Desktop firewall rule
+.PARAMETER DoNotRequireUserLevelAuthentication
+Allows connections from computers running remote desktop
+without Network Level Authentication (not recommended)
+.LINK
+https://boxstarter.org
+
+#>
+	param(
+		[switch]$DoNotRequireUserLevelAuthentication
+	)
+	Write-Output "Enabling Remote Desktop..."
+	$obj = Get-WmiObject -Class "Win32_TerminalServiceSetting" -Namespace root\cimv2\terminalservices
+	if($null -eq $obj) {
+		Write-Output "Unable to locate terminalservices namespace. Remote Desktop is not enabled"
+		return
+	}
+	try {
+		$obj.SetAllowTsConnections(1,1) | Out-Null
+	}
+	catch {
+		throw "There was a problem enabling remote desktop. Make sure your operating system supports remote desktop and there is no group policy preventing you from enabling it."
+	}
+	$obj2 = Get-WmiObject -class Win32_TSGeneralSetting -Namespace root\cimv2\terminalservices -ComputerName . -Filter "TerminalName='RDP-tcp'"
+	if($null -eq $obj2.UserAuthenticationRequired ) {
+		Write-Output "Unable to locate Remote Desktop NLA namespace. Remote Desktop NLA is not enabled"
+		return
+	}
+	try {
+		if($DoNotRequireUserLevelAuthentication) {
+			$obj2.SetUserAuthenticationRequired(0) | Out-Null
+			Write-Output "Disabling Remote Desktop NLA ..."
+		}
+		else {
+			$obj2.SetUserAuthenticationRequired(1) | Out-Null
+			Write-Output "Enabling Remote Desktop NLA ..."
+		}
+	}
+	catch {
+		throw "There was a problem enabling Remote Desktop NLA. Make sure your operating system supports Remote Desktop NLA and there is no group policy preventing you from enabling it."
+	}
+}
+
 ##########
 
 # Set INFN Torino Wallpaper
@@ -186,7 +234,7 @@ End Function
 	}	
 }
 
-function SetINFNWallpaper {
+Function SetINFNWallpaper {
 	Write-Output "Setting INFN Wallpaper..."
 	$WPURI='https://www.to.infn.it/tosegr/INFN%20Torino%20Wallpaper.jpg'
 	$WPFILE='C:\Wallpaper.jpg'
